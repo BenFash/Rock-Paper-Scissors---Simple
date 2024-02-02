52582524e336435d54aef05f9bfe89b42460373b
Ben Fashan <benfashan71@msn.com>
2024-02-02 16:27:04 +0000

    add: javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,37 @@
 // Array with game options//
 const choices = ["rock", "paper", "scissors"];
 
-const resultDisplay = document.getElementById("resultDisplay");
 // added event listeners to all buttons and adds the handle choice functions to it //
 const buttons = document.querySelectorAll("button")
-buttons.forEach(button => button.addEventListener("click, handleChoice"));
+buttons.forEach(button => button.addEventListener("click", handleChoice));
 
 function handleChoice(e) {
-    const userChoice = e.target.button;
-    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-
+    //grabs userchoice// 
+    let userChoice = e.target.id;
+    //generates computer choice// 
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    // updated the choices in HTML 
     document.getElementById("playerDisplay").textContent = `Player: ${userChoice}`;
     document.getElementById("computerDisplay").textContent = `Computer: ${computerChoice}`;
+    //determine results
+    const result = determineWinner(userChoice, computerChoice);
+
+
+    function determineWinner(userChoice, computerChoice) {
+        const winConditions = {
+            rock: ["scissors", "lizard"],
+            paper: ["rock", "spock"],
+            scissors: ["paper", "lizard"],
+            lizard: ["spock", "paper"],
+            spock: ["rock", "scissors"]
+        }
+        //check if user has won and update HTML 
+        if (winConditions[userChoice].includes(computerChoice)) {
+            document.getElementById("resultDisplay").textContent = "You Win👍";
+        } else if (computerChoice === userChoice) {
+            document.getElementById("resultDisplay").textContent = "It's a tie👔";
+        } else {
+            document.getElementById("resultDisplay").textContent = "Computer Win👎";
+        }
+    }
 }
